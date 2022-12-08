28b4b090eb770f9b72de2a2a6613c132d183d351
vicente <vicente.sepuvelda.campos@cftsa.cl>
2022-12-08 19:49:01 -0300

    a

--- examenjm_vs/models.py
+++ examenjm_vs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    Rut=models.CharField(max_length=50)
+    Nombre=models.CharField(max_length=50)
+    Apellido=models.CharField(max_length=50)
+    sector=models.CharField(max_length=50)
+    estado=models.CharField(max_length=50)
