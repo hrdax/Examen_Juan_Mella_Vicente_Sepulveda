1ebbf45bfe24f57bf42e617861816607c8d52a1b
vicente <vicente.sepuvelda.campos@cftsa.cl>
2022-12-08 22:07:04 -0300

    func ver clientes y filtro activos

--- examenjm_vs/urls.py
+++ examenjm_vs/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('crearcliente', views.crearcliente, name='crearcliente'),
+    path('listadoclientes', views.verclientes, name='verclientes'),
+    path('listadoclientes_ac', views.verclientes_activos, name='verclientes_ac'),
     
 ]
