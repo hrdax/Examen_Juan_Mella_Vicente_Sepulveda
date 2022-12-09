e72bbb4dc71a2bc3369029d7021ac9319a451313
vicente <vicente.sepuvelda.campos@cftsa.cl>
2022-12-08 21:20:02 -0300

    funcion insertar cliente

--- examenjm_vs/views.py
+++ examenjm_vs/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from examenjm_vs.models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    sectores = ['Colbún','Panimávida','Maule Sur','La Guardia','San Nicolas','Quinamavida','Rari','Capilla Palacio']
+    estados = ['activo','inactivo']
+    return render(request, 'index.html',{'sector':sectores, 'estado':estados})
+
+def crearcliente(request):
+   rutclient = request.POST['rutid']
+   nameclient = request.POST['nombreid']
+   apellidoclient = request.POST['apellidoid']
+   sectorcliente = request.POST['sectorid']
+   estadoclient = request.POST['estadoid'] 
+   clientenew = Cliente(Rut=rutclient,Nombre=nameclient,Apellido=apellidoclient,sector=sectorcliente,estado=estadoclient)
+   clientenew.save()
+   return render(request,'index.html')
+ 
+ 
